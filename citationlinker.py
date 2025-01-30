2cdc959b5d7ca217a66cd401105edf4944f84769
paradeigma <khk172216@gmail.com>
2025-01-31 00:34:03 +0900

    Modify readme

--- citationlinker.py
+++ citationlinker.py
@@ -6,6 +6,7 @@ import os
 import re
 from dotenv import load_dotenv
 from copy import deepcopy
+from tqdm import tqdm
 
 from langchain_community.document_loaders import UnstructuredPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
@@ -24,6 +25,7 @@ class CitationLinker():
         self.target_id = config['arxiv_id']
         self.preprocess_threhsold = config['preprocess_threhsold']
         self.reference_ratio = config['reference_ratio']
+        self.reference_condition = config['reference_condition']
         self.model = config['model']
 
         self.essay_dir = Path(config['essay_dir'])
@@ -38,8 +40,13 @@ class CitationLinker():
         self.pdf_url = None
 
         self.basic_keys = ["Title", "Authors", "Submitted" ,"Abstract"]
+        self.references = ['References']
+        
         self.content_config = config['content_keys']
         self.content_keys = [value_dict["name"] for _, value_dict in self.content_config.items()]
+        
+        # self.summarize_keys = list(set(self.content_keys) - set(self.references))
+        # self.reference_count_keys = [key for key in self.content_keys if key not in set(self.basic_keys + self.references)]
     
     @staticmethod
     def create_directory_if_not_exists(directory_path):
@@ -71,6 +78,24 @@ class CitationLinker():
                 for chunk in response.iter_content(chunk_size=1024):
                     file.write(chunk)
         print("논문 저장 완료!")
+
+    def _fetch_arxiv_paper(self, title, max_results=30):
+        
+        search = arxiv.Search(
+            query=title,
+            max_results=max_results, 
+            sort_by=arxiv.SortCriterion.Relevance
+        )
+
+        for result in search.results():
+            if title[10:-10].lower().replace(" ", "") in result.title.lower().replace(" ", ""):
+                return ( {
+                    "title": result.title,
+                    "abstract": result.summary,
+                    "pdf_url": result.pdf_url
+                })
+            
+        return None 
 
     def _preprocess(self, save_path):
         # 데이터를 불러와 섹션 별로 나눕니다.
@@ -124,9 +149,6 @@ class CitationLinker():
         return response
 
     def forward(self):
-        result = {
-            "basic_summary" : None,
-        }
 
         # 논문 id를 받아서 논문을 다운 받습니다.
         self._search_arxiv_pdf(arxiv_id=self.target_id)
@@ -143,15 +165,21 @@ class CitationLinker():
         )
 
         # 기본 요약
-        basic_summarize_message = basic_summarize_template.format(essay="\n\n".join([processed_output[key] for key in self.content_keys]))
-        response = CitationLinker._message_to_openai(message=basic_summarize_message)
-        result['basic_summary'] = response.choices[0].message.content,
+        essay = ""
+        for key in self.content_keys:
+            if key not in self.references:
+                for doc in processed_output[key]:
+                    essay += doc.page_content + "\n\n"
+        basic_summarize_message = basic_summarize_template.format(essay=essay)
+        response = CitationLinker._message_to_openai(message=basic_summarize_message, model=self.model)
+        with open(self.result_dir/"basic_summary.json", 'w', encoding="utf-8") as f:
+            json.dump(response.choices[0].message.content, f, ensure_ascii=False, indent=4)
 
-        # 참고문헌 목록화화
-        reference_extraction_message = reference_extraction_template.format(reference=processed_output['References'])
+        # 참고문헌 목록화
+        reference_extraction_message = reference_extraction_template.format(references=processed_output['References'])
         flag = True
         while flag:
-            response = CitationLinker._message_to_openai(message=reference_extraction_message)
+            response = CitationLinker._message_to_openai(message=reference_extraction_message, model=self.model)
             text = response.choices[0].message.content
             text = re.sub("```json","",text)
             text = re.sub("```","",text)
@@ -166,14 +194,13 @@ class CitationLinker():
         processed_output['References'] = deepcopy(reference_dict)
 
         # 인용횟수 counting
-        n = 5
-        for index in range(n):
+        for index in range(len(reference_count_template_dict)):
             result = []
             for key in self.content_keys:
-                if key not in self.basic_keys:
+                if key not in self.basic_keys + self.references:
                     for essay in processed_output[key]:
-                        reference_count_message = reference_count_template_dict[str(index)].format(references=reference_dict, essay=essay)
-                        response = CitationLinker._message_to_openai(reference_count_message)
+                        reference_count_message = reference_count_template_dict[str(index)].format(references=reference_dict, essay=essay, condition=self.reference_condition)
+                        response = CitationLinker._message_to_openai(reference_count_message, model=self.model)
                         try:
                             text = response.choices[0].message.content
                             text = re.sub("```json","",text)
@@ -188,7 +215,84 @@ class CitationLinker():
                 for key, value_dict in data.items():
                     processed_output["References"][key]['Counter'] += value_dict['Counter']
                     processed_output["References"][key]['Context'].extend(value_dict['Context'])
-        print(processed_output)
+
+        # reference 논문 다운 받아오기
+        for index in range(len(processed_output['References'])):
+            title = processed_output['References'][str(index+1)]['Title']
+            try :
+                paper_info = self._fetch_arxiv_paper(title)
+                if paper_info is None:
+                    paper_info = self._fetch_arxiv_paper(title, 150)
+            except Exception as e:
+                print(index+1,"번째 논문 예외 발생: ", e)
+                try :
+                    paper_info = self._fetch_arxiv_paper(title, None)
+                except:
+                    paper_info = None
+
+            if paper_info is not None:
+                pdf_url = paper_info['pdf_url']
+                abstract = paper_info['abstract']
+                processed_output['References'][str(index+1)]['abstract'] = abstract
+                processed_output['References'][str(index+1)]['pdf_url'] = pdf_url
+                save_path = self.essay_dir / (str(index+1)+ "-" + paper_info['title'][:15]+".pdf")
+                self._download_arxiv_pdf(pdf_url, save_path)
+                print(index+1,"번째 논문 다운로드 완료")
+            
+            else:
+                pdf_url = None
+                abstract = None
+                processed_output['References'][str(index+1)]['abstract'] = abstract
+                processed_output['References'][str(index+1)]['pdf_url'] = pdf_url
+                print(index+1,"번째 논문 다운로드 실패")
+                print(f"    {processed_output['References'][str(index+1)]['Title']}")
+
+        # 논문 다운로드 후, 질문 축소
+        filtered_reference_dict = { key: value for key, value in processed_output['References'].items() if value['pdf_url'] is not None}
+        nums = int(round(len(processed_output['References'])*self.reference_ratio, 0))
+        related_reference = dict(sorted(filtered_reference_dict.items(), key=lambda x:x[1]['Counter'], reverse=True)[:nums])
+        total_related_reference = dict(sorted(filtered_reference_dict.items(), key=lambda x:x[1]['Counter'], reverse=True))
+        # print(total_related_reference)
+        # result['total_reference_result_dict'] = total_related_reference
+        with open(self.result_dir/"reference_count.json", 'w', encoding="utf-8") as f:
+            json.dump(total_related_reference, f, ensure_ascii=False, indent=4)
+
+        for index in related_reference.keys():
+            query_list = related_reference[index]['Context']
+            user_message = question_reduction_template.format(text_list=query_list)
+            response = CitationLinker._message_to_openai(user_message, model=self.model)
+            related_reference[index]['Questions'] = response.choices[0].message.content
+
+        # reference와의 접점을 찾기 위한 요약
+        main_essay = ""
+        for key in self.basic_keys:
+            for doc in processed_output[key]:
+                main_essay += (doc.page_content + "\n\n")
+
+        for index in tqdm(related_reference.keys(), desc="인용 논문과의 관련 지점 정리..."):
+            title = related_reference[index]['Title']
+            questions = related_reference[index]['Questions']
+        
+            for path in self.essay_dir.rglob("*.pdf"):
+                if path.name.split("-")[0] == index:
+                    break
+        
+            loader = UnstructuredPDFLoader(path)
+            documents = loader.load()
+            essay = documents[0].page_content
+            essay = "\n".join([text for text in essay.split("\n") if len(text) >= self.preprocess_threhsold])
+            reference_qna_message = reference_qna_template.format(essay = essay, questions=questions, title=title)
+            response = CitationLinker._message_to_openai(reference_qna_message, model=self.model)
+            summary = response.choices[0].message.content
+            related_reference[index]['Summary'] = summary
+            
+            research_progress_message = research_progress_template.format(title=processed_output['Title'], essay=main_essay, qna = summary)
+            response = CitationLinker._message_to_openai(research_progress_message,model=self.model)
+            summary_qna = response.choices[0].message.content
+            related_reference[index]['Summary_QnA'] = summary_qna
+
+        with open(self.result_dir/"reference_qna.json", 'w', encoding="utf-8") as f:
+            json.dump(related_reference, f, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
 
@@ -197,5 +301,3 @@ if __name__ == "__main__":
 
     citation_linker = CitationLinker(config)
     citation_linker.forward()
-
-
